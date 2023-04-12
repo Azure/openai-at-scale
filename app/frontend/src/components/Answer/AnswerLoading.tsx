fc98d3e13d261a3812436c41b49bc081d9d4cb99
Keita Onabuta <keonabut@microsoft.com>
2023-04-12 17:07:53 +0900

    delete unnecesarry files

--- app/frontend/src/components/Answer/AnswerLoading.tsx
+++ app/frontend/src/components/Answer/AnswerLoading.tsx
@@ -14,12 +14,7 @@ export const AnswerLoading = () => {
     return (
         <animated.div style={{ ...animatedStyles }}>
             <Stack className={styles.answerContainer} verticalAlign="space-between">
-                {/* <AnswerIcon /> */}
                 <Stack.Item grow>
-                    {/* <p className={styles.answerText}>
-                        <span className={styles.loadingdots} />
-                    </p> */}
-                    {/* <AnswerIcon /> */}
                     <ProgressIndicator label="Finding answer ..." barHeight={barHeight} />
                 </Stack.Item>
             </Stack>
--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -1,31 +1,17 @@
 import { useRef, useState, useEffect } from "react";
-import { Checkbox, Panel, DefaultButton, TextField, SpinButton, Slider } from "@fluentui/react";
+import { TextField, Slider } from "@fluentui/react";
 import { Stack } from "@fluentui/react";
-import { useId, Input, Label } from "@fluentui/react-components";
-import { AnimalCatRegular, TopSpeed20Filled } from "@fluentui/react-icons";
-import { UserChatMessageIcon } from "../../components/UserChatMessage/UserChatMessageIcon";
-
+import { Label } from "@fluentui/react-components";
 import styles from "./Chat.module.css";
 
 import { chatApi, Approaches, AskResponse, ChatRequest, ChatTurn } from "../../api";
 import { Answer, AnswerError, AnswerLoading } from "../../components/Answer";
 import { QuestionInput } from "../../components/QuestionInput";
-import { ExampleList } from "../../components/Example";
 import { UserChatMessage } from "../../components/UserChatMessage";
-import { AnalysisPanel, AnalysisPanelTabs } from "../../components/AnalysisPanel";
-import { SettingsButton } from "../../components/SettingsButton";
 import { ClearChatButton } from "../../components/ClearChatButton";
 
 const Chat = () => {
-    const outlineId = useId("input-outline");
-    const [isConfigPanelOpen, setIsConfigPanelOpen] = useState(false);
-    const [promptTemplate, setPromptTemplate] = useState<string>("");
     const [promptSystemTemplate, setPromptSystemTemplate] = useState<string>("");
-    const [retrieveCount, setRetrieveCount] = useState<number>(3);
-    const [useSemanticRanker, setUseSemanticRanker] = useState<boolean>(true);
-    const [useSemanticCaptions, setUseSemanticCaptions] = useState<boolean>(false);
-    const [excludeCategory, setExcludeCategory] = useState<string>("");
-    const [useSuggestFollowupQuestions, setUseSuggestFollowupQuestions] = useState<boolean>(false);
     const [topp, setTopp] = useState<number>(0.95);
     const [temperature, setTemperature] = useState<number>(0.5);
 
@@ -35,10 +21,6 @@ const Chat = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<unknown>();
 
-    const [activeCitation, setActiveCitation] = useState<string>();
-    const [activeAnalysisPanelTab, setActiveAnalysisPanelTab] = useState<AnalysisPanelTabs | undefined>(undefined);
-
-    const [selectedAnswer, setSelectedAnswer] = useState<number>(0);
     const [answers, setAnswers] = useState<[user: string, response: AskResponse][]>([]);
 
     const makeApiRequest = async (question: string) => {
@@ -47,23 +29,14 @@ const Chat = () => {
 
         error && setError(undefined);
         setIsLoading(true);
-        setActiveCitation(undefined);
-        setActiveAnalysisPanelTab(undefined);
 
         try {
             const history: ChatTurn[] = answers.map(a => ({ user: a[0], bot: a[1].answer }));
             const request: ChatRequest = {
                 history: [...history, { user: question, bot: undefined }],
                 approach: Approaches.ReadRetrieveRead,
-                // Developer Configuration の設定値
                 overrides: {
-                    promptTemplate: promptTemplate.length === 0 ? undefined : promptTemplate,
                     promptSytemTemplate: promptSystemTemplate.length === 0 ? undefined : promptSystemTemplate,
-                    excludeCategory: excludeCategory.length === 0 ? undefined : excludeCategory,
-                    top: retrieveCount,
-                    semanticRanker: useSemanticRanker,
-                    semanticCaptions: useSemanticCaptions,
-                    suggestFollowupQuestions: useSuggestFollowupQuestions,
                     temperature: temperature
                 }
             };
@@ -81,76 +54,18 @@ const Chat = () => {
     const clearChat = () => {
         lastQuestionRef.current = "";
         error && setError(undefined);
-        setActiveCitation(undefined);
-        setActiveAnalysisPanelTab(undefined);
         setAnswers([]);
     };
 
     useEffect(() => chatMessageStreamEnd.current?.scrollIntoView({ behavior: "smooth" }), [isLoading]);
 
-    const onPromptTemplateChange = (_ev?: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-        setPromptTemplate(newValue || "");
-    };
-
     const onPromptSystemTemplateChange = (_ev?: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
         setPromptSystemTemplate(newValue || "");
     };
 
-    const onRetrieveCountChange = (_ev?: React.SyntheticEvent<HTMLElement, Event>, newValue?: string) => {
-        setRetrieveCount(parseInt(newValue || "3"));
-    };
-
-    const onTemperatureChange = (_ev?: React.SyntheticEvent<HTMLElement, Event>, newValue?: string) => {
-        setTemperature(parseInt(newValue || "0.5"));
-    };
-
-    const onTemperatureChange2 = (_ev?: React.SyntheticEvent<HTMLElement, Event>, newValue?: number) => {
-        setTemperature(newValue || 0.5);
-    };
     const temperatureOnChange = (value: number) => setTemperature(value);
 
     const toppOnChange = (value: number) => setTopp(value);
-
-    const onUseSemanticRankerChange = (_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
-        setUseSemanticRanker(!!checked);
-    };
-
-    const onUseSemanticCaptionsChange = (_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
-        setUseSemanticCaptions(!!checked);
-    };
-
-    const onExcludeCategoryChanged = (_ev?: React.FormEvent, newValue?: string) => {
-        setExcludeCategory(newValue || "");
-    };
-
-    const onUseSuggestFollowupQuestionsChange = (_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
-        setUseSuggestFollowupQuestions(!!checked);
-    };
-
-    const onExampleClicked = (example: string) => {
-        makeApiRequest(example);
-    };
-
-    const onShowCitation = (citation: string, index: number) => {
-        if (activeCitation === citation && activeAnalysisPanelTab === AnalysisPanelTabs.CitationTab && selectedAnswer === index) {
-            setActiveAnalysisPanelTab(undefined);
-        } else {
-            setActiveCitation(citation);
-            setActiveAnalysisPanelTab(AnalysisPanelTabs.CitationTab);
-        }
-
-        setSelectedAnswer(index);
-    };
-
-    const onToggleTab = (tab: AnalysisPanelTabs, index: number) => {
-        if (activeAnalysisPanelTab === tab && selectedAnswer === index) {
-            setActiveAnalysisPanelTab(undefined);
-        } else {
-            setActiveAnalysisPanelTab(tab);
-        }
-
-        setSelectedAnswer(index);
-    };
 
     return (
         <div className={styles.container}>
@@ -164,16 +79,7 @@ const Chat = () => {
                                     <div key={index}>
                                         <UserChatMessage message={answer[0]} />
                                         <div className={styles.chatMessageGpt}>
-                                            <Answer
-                                                key={index}
-                                                answer={answer[1]}
-                                                isSelected={selectedAnswer === index && activeAnalysisPanelTab !== undefined}
-                                                onCitationClicked={c => onShowCitation(c, index)}
-                                                onThoughtProcessClicked={() => onToggleTab(AnalysisPanelTabs.ThoughtProcessTab, index)}
-                                                onSupportingContentClicked={() => onToggleTab(AnalysisPanelTabs.SupportingContentTab, index)}
-                                                onFollowupQuestionClicked={q => makeApiRequest(q)}
-                                                showFollowupQuestions={useSuggestFollowupQuestions && answers.length - 1 === index}
-                                            />
+                                            <Answer key={index} answer={answer[1]}></Answer>
                                         </div>
                                     </div>
                                 ))}
@@ -206,17 +112,6 @@ const Chat = () => {
                                 />
                             </div>
                         </div>
-
-                        {answers.length > 0 && activeAnalysisPanelTab && (
-                            <AnalysisPanel
-                                className={styles.chatAnalysisPanel}
-                                activeCitation={activeCitation}
-                                onActiveTabChanged={x => onToggleTab(x, selectedAnswer)}
-                                citationHeight="810px"
-                                answer={answers[selectedAnswer][1]}
-                                activeTab={activeAnalysisPanelTab}
-                            />
-                        )}
                     </div>
                 </Stack.Item>
                 <Stack.Item grow align="stretch" className={styles.stackItemConfig}>

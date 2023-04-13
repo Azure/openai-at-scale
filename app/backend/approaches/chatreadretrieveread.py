46b267fae062e2663e1653d085cdff8be6b4bd7d
Keita Onabuta <keonabut@microsoft.com>
2023-04-13 16:58:15 +0900

    include all past pairs of message into prompt

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -24,31 +24,39 @@ class ChatReadRetrieveReadApproach(Approach):
         filter = "category ne '{}'".format(exclude_category.replace("'", "''")) if exclude_category else None
         temperature = overrides.get("temperature") or 0.5
         promptSystemTemplate = overrides.get("prompt_system_template") or "You are an AI assistant that helps people find information."
-        # print(openai.Model.list())
         # Step
         system_prompt_template = {}
         system_prompt_template["role"] = "system"
         system_prompt_template["content"] = promptSystemTemplate
-        user_prompt_template = {}
-        user_prompt_template["role"] = "user"
-        user_prompt_template["content"] = history[-1]["user"]
-        print([system_prompt_template, user_prompt_template])
+        print("prompt:",[system_prompt_template]+self.get_chat_history_as_text(history))
+
         completion = openai.ChatCompletion.create(
             engine=self.chatgpt_deployment,
-            messages = [system_prompt_template, user_prompt_template],
+            messages = [system_prompt_template]+self.get_chat_history_as_text(history),
             temperature=temperature,
             max_tokens=800,
             top_p=0.95,
             frequency_penalty=0,
             presence_penalty=0,
             stop=None)
-        print(completion)
-        return {"data_points": [], "answer": completion.choices[0].message["content"], "thoughts": None}
+        print("completion: ", completion)
+        return {"answer": completion.choices[0].message["content"]}
     
-    def get_chat_history_as_text(self, history, include_last_turn=True, approx_max_tokens=1000) -> str:
-        history_text = ""
-        for h in reversed(history if include_last_turn else history[:-1]):
-            history_text = """<|im_start|>user""" +"\n" + h["user"] + "\n" + """<|im_end|>""" + "\n" + """<|im_start|>assistant""" + "\n" + (h.get("bot") + """<|im_end|>""" if h.get("bot") else "") + "\n" + history_text
-            if len(history_text) > approx_max_tokens*4:
-                break    
+    def get_chat_history_as_text(self, history, include_last_turn=True, approx_max_tokens=1000) -> list:
+        history_text = []
+        for h in history:
+            user_text = {}
+            user_text["role"] = "user"
+            user_text["content"] = h["user"]
+
+            if h.get("bot") is None:
+                history_text = history_text + [user_text]
+            else:
+                bot_text = {}
+                bot_text["role"] = "assistant"
+                bot_text["content"] = h.get("bot")
+                history_text = history_text + [user_text, bot_text]
+            # if len(history_text) > approx_max_tokens*4:
+            #     break
+        
         return history_text

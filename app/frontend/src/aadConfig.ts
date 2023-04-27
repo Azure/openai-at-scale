e95246c757e82c42677511e89523bb64f69f33a4
Kyohei MORIYAMA <sasukeh@users.noreply.github.com>
2023-04-27 12:34:41 +0900

    Create aadConfig.ts

---
+++ app/frontend/src/aadConfig.ts
@@ -0,0 +1,6 @@
+export const aadConfig = {
+    clientId: "<your clientid>",
+    tenantId: "<your tenantid>",
+    redirectUri: "http://localhost:5000",
+    authorityBaseUrl: "https://login.microsoftonline.com/"
+};

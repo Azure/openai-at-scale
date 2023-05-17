da197e68c2f9769a96df73b56db2610db1c55aa4
Keita Onabuta <keonabut@microsoft.com>
2023-05-17 16:51:40 +0000

    delete unnecesarry print etc

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,5 +1,4 @@
 import os
-import json
 import logging
 import openai
 from flask import Flask, request, jsonify
@@ -15,21 +14,10 @@ AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "myopenai"
 AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "davinci"
 AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "chat"
 
-# Use the current user identity to authenticate with Azure OpenAI, Cognitive Search and Blob Storage (no secrets needed, 
-# just use 'az login' locally, and managed identity when deployed on Azure). If you need to use keys, use separate AzureKeyCredential instances with the 
-# keys for each service
-# If you encounter a blocking error during a DefaultAzureCredntial resolution, you can exclude the problematic credential by using a parameter (ex. exclude_shared_token_cache_credential=True)
-azure_credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
-
 # Used by the OpenAI SDK
 openai.api_type = "azure"
 openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
 openai.api_version = "2023-03-15-preview"
-
-# Comment these two lines out if using keys, set your API key in the OPENAI_API_KEY environment variable instead
-# openai.api_type = "azure_ad"
-# openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
-# openai.api_key = openai_token.token
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 chat_approaches = {
@@ -46,7 +34,7 @@ def static_file(path):
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    print("/chat request", request.json)
+
     # ensure_openai_token()
     approach = request.json["approach"]
     try:
--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -11,10 +11,6 @@ class ChatReadRetrieveReadApproach(Approach):
         self.gpt_deployment = gpt_deployment
 
     def run(self, history: list[dict], overrides: dict, sessionConfig: dict, userInfo:dict, header: dict) -> any:
-        print("history:", history)
-        print("override:", overrides)
-        print("sessionConfig:", sessionConfig)
-        print("userInfo:", userInfo)
         top_p = overrides.get("top") or 0.95
         temperature = overrides.get("temperature") or 0.7
         max_tokens = overrides.get("maxResponse") or 800
@@ -22,13 +18,15 @@ class ChatReadRetrieveReadApproach(Approach):
         pastMessages = sessionConfig.get("pastMessages") or 10
         user_name= userInfo.get("username") or "anonymous user_name"
         user_id = userInfo.get("email") or "anonymous user_id"
-        chat_session_id = header.get("SessionId") or "anonymous-" + str(uuid.uuid4())
+        chat_session_id = header.get("Sessionid") or "anonymous-" + str(uuid.uuid4())
+        print("user:", {"name": user_name,"user_id":user_id} ) # For Azure Log Analytics
+        print("parameters:", {"Max Response": max_tokens, "Temperature": temperature, "Top P": top_p, "Past message included": pastMessages})
 
         # Step
         system_prompt_template = {}
         system_prompt_template["role"] = "system"
         system_prompt_template["content"] = promptSystemTemplate
-        print("prompt:",[system_prompt_template]+self.get_chat_history_as_text(history, pastMessages))
+        print("prompt:",[system_prompt_template]+self.get_chat_history_as_text(history, pastMessages)) # For Azure Log Analytics
 
         completion = openai.ChatCompletion.create(
             engine=self.chatgpt_deployment,
@@ -39,17 +37,17 @@ class ChatReadRetrieveReadApproach(Approach):
             frequency_penalty=0,
             presence_penalty=0,
             stop=None)
-        print("completion: ", completion)
+        print("completion: ", completion) # For Azure Log Analytics
 
         document_definition = { "id": str(uuid.uuid4()),
                                 "chat_session_id": chat_session_id, 
-                            "user": {"name": user_name,"user_id":user_id}, 
-                            'message': {"id":completion.get("id") or "anonymous-id",
+                                "user": {"name": user_name,"user_id":user_id}, 
+                                'message': {"id":completion.get("id") or "anonymous-id",
                                         "prompt":[system_prompt_template]+self.get_chat_history_as_text(history, pastMessages),
                                         "other_attr":[{"completion": completion}],
                                         "previous_message_id":"previous_message_id"}}
         
-        cosmosdb_logging.insert_chat_log(document_definition)
+        cosmosdb_logging.insert_chat_log(document_definition) # Store prompt log data into Azure Cosmos DB
         return {"answer": completion.choices[0].message["content"]}
     
 
